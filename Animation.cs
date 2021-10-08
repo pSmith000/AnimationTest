941bb45b5adfc9add5555d987b078303ad7b0cca
Peyton <peytonsemail2000@gmail.com>
2021-10-08 00:10:50 -0500

    Created Background Animation

---
+++ Animation.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AnimationTest
+{
+    class Animation
+    {
+        public void UpMovement()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("    0 ");
+            Console.WriteLine(@"   \|/ ");
+            Console.WriteLine("    | ");
+            Console.WriteLine(@"   / \ ");
+            Console.WriteLine("         ");
+            Thread.Sleep(200);
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("    0 ");
+            Console.WriteLine(@"   /|\ ");
+            Console.WriteLine("    | ");
+            Console.WriteLine(@"   | | ");
+            Thread.Sleep(200);
+        }
+
+        public void DownMovement()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("             ");
+            Console.WriteLine(@"     0 ");
+            Console.WriteLine(@"    |\ ");
+            Console.WriteLine(@"    _| ");
+            Thread.Sleep(200);
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("    0 ");
+            Console.WriteLine(@"   /|\ ");
+            Console.WriteLine("    | ");
+            Console.WriteLine(@"   | | ");
+            Thread.Sleep(200);
+        }
+
+        public void LeftMovement()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("  0        ");
+            Console.WriteLine(@"//|       ");
+            Console.WriteLine("  |        ");
+            Console.WriteLine(@" | |      ");
+            Thread.Sleep(200);
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("    0 ");
+            Console.WriteLine(@"   /|\ ");
+            Console.WriteLine("    | ");
+            Console.WriteLine(@"   | | ");
+            Thread.Sleep(200);
+        }
+
+        public void RightMovement()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("      0 ");
+            Console.WriteLine(@"      |\\ ");
+            Console.WriteLine("      | ");
+            Console.WriteLine(@"     | | ");
+            Thread.Sleep(200);
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("             ");
+            Console.WriteLine("    0        ");
+            Console.WriteLine(@"   /|\      ");
+            Console.WriteLine("    |        ");
+            Console.WriteLine(@"   | |      ");
+            Thread.Sleep(200);
+        }
+
+        public void Background()
+        {
+
+            for (int i = 89; i > 0; i--)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("          .-~~~-.  ");
+                Console.SetCursorPosition(i, 1);
+                Console.Write("  .- ~ ~-(       )_ _  ");
+                Console.SetCursorPosition(i, 2);
+                Console.Write(" /                     ~ -.  ");
+                Console.SetCursorPosition(i, 3);
+                Console.Write(@"|                           \  ");
+                Console.SetCursorPosition(i, 4);
+                Console.Write(@" \                         .'  ");
+                Console.SetCursorPosition(i, 5);
+                Console.Write("  ~- . _____________ . -~  ");
+
+                Thread.Sleep(500);
+
+                Console.SetCursorPosition(i + 30, 8);
+                Console.Write("        # #### ####  ");
+                Console.SetCursorPosition(i + 30, 9);
+                Console.Write(@"      ### \/#|### |/####  ");
+                Console.SetCursorPosition(i + 30, 10);
+                Console.Write(@"     ##\/#/ \||/##/_/##/_#  ");
+                Console.SetCursorPosition(i + 30, 11);
+                Console.Write(@"   ###  \/###|/ \/ # ###  ");
+                Console.SetCursorPosition(i + 30, 12);
+                Console.Write(@" ##_\_#\_\## | #/###_/_####  ");
+                Console.SetCursorPosition(i + 30, 13);
+                Console.Write(@"## #### # \ #| /  #### ##/##  ");
+                Console.SetCursorPosition(i + 30, 14);
+                Console.Write(" __#_--###`  |{,###---###-~  ");
+                Console.SetCursorPosition(i + 30, 15);
+                Console.Write(@"           \ }{  ");
+                Console.SetCursorPosition(i + 30, 16);
+                Console.Write("            }}{  ");
+                Console.SetCursorPosition(i + 30, 17);
+                Console.Write("            }}{  ");
+                Console.SetCursorPosition(i + 30, 18);
+                Console.Write("            {{}  ");
+                Console.SetCursorPosition(i + 30, 19);
+                Console.Write("      , -=-~{ .-^- _  ");
+
+                Console.SetCursorPosition(0, 20);
+                Console.Write(new string( '_', 120));
+
+                if (i == 1)
+                {
+                    i = 90;
+                    Console.Clear();
+                }
+                    
+
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace AnimationTest
+{
+    class Game
+    {
+        bool gameOver = false;
+        private Animation _playerAnimation;
+
+        public void Run()
+        {
+            if (!gameOver)
+                Update();
+        }
+        void Update()
+        {
+
+            Console.SetCursorPosition(0, 10);
+            Console.WriteLine("             ");
+            Console.WriteLine("    0 ");
+            Console.WriteLine(@"   /|\ ");
+            Console.WriteLine("    | ");
+            Console.WriteLine(@"   | | ");
+
+            _playerAnimation = new Animation();
+
+            _playerAnimation.Background();
+
+            ConsoleKeyInfo cki;
+
+            Console.TreatControlCAsInput = true;
+
+            cki = Console.ReadKey();
+
+            while (cki.Key != ConsoleKey.Escape)
+            {
+
+                if ((cki.Key.ToString() == "UpArrow"))
+                {
+                    _playerAnimation.UpMovement();
+                }
+                else if ((cki.Key.ToString() == "DownArrow"))
+                {
+                    _playerAnimation.DownMovement();
+                }
+                else if ((cki.Key.ToString() == "LeftArrow"))
+                {
+                    _playerAnimation.LeftMovement();
+                }
+                else if ((cki.Key.ToString() == "RightArrow"))
+                {
+                    _playerAnimation.RightMovement();
+                }
+                else if ((cki.Key.ToString() == "Spacebar"))
+                {
+                    _playerAnimation.Background();
+                }
+
+                cki = Console.ReadKey();
+            }
+        }
+    }
+}
